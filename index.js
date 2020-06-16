0f9d5289e16d20e7327afd6c40ca702a21087040
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2020-06-16 10:38:26 -0300

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// Calculo IMC 
+
+const nome = "Carlos"
+const peso = 84
+const altura = 1.88
+const sexo = "masculino"
+
+const imc = peso / (altura * altura) 
+
+
+if (imc >= 30) {
+    console.log ('Carlos você está acima do peso')
+} 
+    else {
+    console.log ('carlos você não está acima do peso')
+}
+
+
+
+
+
