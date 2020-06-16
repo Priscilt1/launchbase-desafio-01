ed781589c8ff9b7ca949e225484a050a36d78738
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2020-06-16 12:14:15 -0300

    inserção do desafio de calculo da  aposentadoria

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
-// Calculo IMC 
+// Cálculo IMC 
 
-const nome = "Carlos"
+const nome1 = "Carlos"
 const peso = 84
 const altura = 1.88
-const sexo = "masculino"
+const sexo1 = "masculino"
 
 const imc = peso / (altura * altura) 
 
 
 if (imc >= 30) {
-    console.log ('Carlos você está acima do peso')
+    console.log (`${nome1}, você está acima do peso`)
 } 
     else {
-    console.log ('carlos você não está acima do peso')
+    console.log (`${nome1},você não está acima do peso`)
 }
 
+
+
+
+
+//Cálculo de aposentadoria 
+
+const nome = "Silvana"
+const sexo = "F"
+const idade = 48
+const contribuicao = 23
+
+if ((sexo == 'F' )) {
+    if ((contribuicao + idade >=85 || contribuicao >=30)) {
+        console.log (`${nome}, você pode se aposentar!`)
+    } else {
+        console.log (`${nome}, você não pode se aposentar!`)
+    }
+} 
+
+
+if ((sexo == 'M' )) {
+    if ((contribuicao + idade >=95 || contribuicao >=35)) {
+        console.log (`${nome}, você pode se aposentar!`)
+    } else {
+        console.log (`${nome}, você não pode se aposentar!`)
+    }
+} 
 
 
 
